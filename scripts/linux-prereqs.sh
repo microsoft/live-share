96b6aa9f334b63c40512600131fa407d6f4a39b9
Nhoya <nhoyadx@gmail.com>
2018-04-27 18:38:48 +0200

    Minor fixes

--- scripts/linux-prereqs.sh
+++ scripts/linux-prereqs.sh
@@ -28,21 +28,21 @@ echo ""
 APT=$(which apt 2>&1)
 if type apt > /dev/null 2>&1; then
     echo "(*) Detected Debian / Ubuntu"
-    sudo $APT -yq install libunwind8 liblttng-ust0 libcurl3 libssl1.0.0 libuuid1 libkrb5-3 zlib1g gnome-keyring libsecret-1-0 desktop-file-utils gettext apt-transport-https
+    sudo apt -yq install libunwind8 liblttng-ust0 libcurl3 libssl1.0.0 libuuid1 libkrb5-3 zlib1g gnome-keyring libsecret-1-0 desktop-file-utils gettext apt-transport-https
     if [ $? -ne 0 ]; then
-        "(!) Installation failed!" 
+        echo "(!) Installation failed!" 
         exit 1
     fi
 
     LIBICU=$(apt-cache search libicu | grep -o "libicu[0-9][0-9]\s")
     if [ $? -ne 0 ]; then
-        "(!) Installation failed!" 
+        echo "(!) Installation failed!" 
         exit 1
     fi
     echo "(i) libicu package is $LIBICU"
     sudo apt -yq install $LIBICU
     if [ $? -ne 0 ]; then
-        "(!) Installation failed!" 
+        echo "(!) Installation failed!" 
         exit 1; 
     fi
 
@@ -51,7 +51,7 @@ elif type yum  > /dev/null 2>&1; then
     echo "(*) Detected RHL / Fedora / CentOS"
     sudo yum -y install libunwind lttng-ust libcurl openssl-libs libuuid krb5-libs libicu zlib gnome-keyring libsecret desktop-file-utils    #Find libicu version
     if [ $? -ne 0 ]; then
-        "(!) Installation failed!" 
+        echo "(!) Installation failed!" 
         exit 1
     fi
 
